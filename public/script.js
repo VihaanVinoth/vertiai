392c44ae5fca02db8650b751518e32c281531d2f
Vihaan Vinoth <vertialX1000@outlook.com.au>
2026-05-11 21:58:49 +1000

    fix login

--- public/script.js
+++ public/script.js
@@ -6,8 +6,6 @@ const clearBtn = document.getElementById("clear-btn");
 const supabaseUrl = "https://qnahvrzqewdgewdjqbef.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuYWh2cnpxZXdkZ2V3ZGpxYmVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0OTU3MzgsImV4cCI6MjA5NDA3MTczOH0.NJ6jqu2j1dir65HEPC6xVsgY5L-_PhtuPn9i2kAX-aM";
 
-const loginBtn = document.getElementById("google-login");
-
 const supabase = window.supabase.createClient(
   supabaseUrl,
   supabaseKey
@@ -25,7 +23,6 @@ if (loginBtn) {
 
 async function checkUser() {
   const { data: { user } } = await supabase.auth.getUser();
-  console.log(user);
 }
 
 checkUser();
