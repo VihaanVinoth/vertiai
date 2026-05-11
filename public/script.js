9edb3c25f717dd2930e5b9acee321dbbfa938d0f
Vihaan Vinoth <vertialX1000@outlook.com.au>
2026-05-11 21:51:48 +1000

    add login

--- public/script.js
+++ public/script.js
@@ -3,6 +3,28 @@ const input = document.getElementById("user-input");
 const sendBtn = document.getElementById("deliver-btn");
 const clearBtn = document.getElementById("clear-btn");
 
+const supabaseUrl = "https://qnahvrzqewdgewdjqbef.supabase.co/rest/v1/";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuYWh2cnpxZXdkZ2V3ZGpxYmVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0OTU3MzgsImV4cCI6MjA5NDA3MTczOH0.NJ6jqu2j1dir65HEPC6xVsgY5L-_PhtuPn9i2kAX-aM";
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+
+const loginBtn = ocument.getElementById("google-login");
+
+loginBtn.addEventListener("click", async () => {
+  await supabase.auth.signInWithOAuth({
+    provider: "google"
+  });
+});
+
+const {
+  data: { user }
+} = await supabase.auth.getUser();
+
+console.log(user);
+
 function toLatex(text) {
   return text
     .replace(/(\w+)\/(\w+)/g, "$$\\frac{$1}{$2}$$")
