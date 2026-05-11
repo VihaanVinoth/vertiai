eac726ad3890df0afff9931713eb2a11c3aa7f0d
Vihaan Vinoth <vertialX1000@outlook.com.au>
2026-05-11 22:51:45 +1000

    fix

--- public/script.js
+++ public/script.js
@@ -43,7 +43,7 @@ async function sendMessage() {
 
   input.value = "";
 
-  const response = await fetch("http://vertai.onrender.com", {
+  const response = await fetch("http://vertai.onrender.com/chat", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
